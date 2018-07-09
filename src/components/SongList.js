39dff5af8b5c75d73d99a6409033e6041a856dc5
Justin Awrey <awreyjustin@gmail.com>
2018-07-08 22:27:21 -0700

    add pulsing message when no songs exist in queue

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { connect } from 'react-redux'
 import SongContainer from '../containers/SongContainer'
 
@@ -8,13 +8,27 @@ const StyledSongList = styled.ul`
     background-color: ${({theme}) => theme.secondary.dark};
 `
 
-const SongList = ({songs, handleClick}) => (
-    <StyledSongList>
-        {songs.map(id => <SongContainer key={id} id={id}/>)}
-    </StyledSongList>
+const pulse = keyframes`
+    0% {font-size: 1em;}
+    50% {font-size: 1.3em;}
+    100% {font-size: 1em;}
+`
+
+const NoSongs = styled.div`
+    text-align: center;
+    background-color: ${({theme}) => theme.secondary.dark};
+    animation: ${pulse} 3s ease-in-out infinite;
+`
+
+const SongList = ({songs}) => (
+     songs.length > 0 ?
+        <StyledSongList>
+            {songs.map(id => <SongContainer key={id} id={id}/>)}
+        </StyledSongList> : 
+        <NoSongs>
+            <h1>awww shit... there's nothing here!</h1>
+            waiting for new songs to be queued...
+        </NoSongs>
 )
 
 export default connect(state => ({songs: state.songs}))(SongList)
-
-
-    
