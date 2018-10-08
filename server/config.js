031b2663579461fb7d872a6db1c06d2fb08175b4
Justin Awrey <awreyjustin@gmail.com>
2018-10-08 12:50:15 -0700

    add dotenv to dependencies

--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv'
+
 let config
 if (process.env.NODE_ENV === 'production') {
     config = process.env
 } else {
-    import dotenv from 'dotenv'
     const { parsed, error } = dotenv.config()
     if (error) {
         throw 'Could not parse .env file'
