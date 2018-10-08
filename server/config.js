0498a01be9b2f5658b52052765980f8aaa8ef8a8
Justin Awrey <awreyjustin@gmail.com>
2018-10-08 12:46:10 -0700

    dont import dotenv in prod

--- server/config.js
+++ server/config.js
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv'
-
 let config
 if (process.env.NODE_ENV === 'production') {
     config = process.env
 } else {
+    import dotenv from 'dotenv'
     const { parsed, error } = dotenv.config()
     if (error) {
         throw 'Could not parse .env file'
