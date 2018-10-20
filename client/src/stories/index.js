e224f6bae8bbc56c701cd2e9580af2266293d970
Justin Awrey <awreyjustin@gmail.com>
2018-10-19 22:44:43 -0700

    upgrade storybook to v4 and cra to c2b

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -1,68 +1,11 @@
 import React from 'react'
-import { injectGlobal, ThemeProvider } from 'styled-components'
-import { Provider } from 'react-redux'
 import { storiesOf } from '@storybook/react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-    faTimes,
-    faMobileAlt,
-    faForward,
-    faBackward,
-    faPlayCircle,
-    faPauseCircle,
-} from '@fortawesome/free-solid-svg-icons'
+import { ThemeProvider } from 'styled-components'
 
-import Playing from '../components/Playing'
-import SongList from '../components/SongList'
-import initialState, { createInitialStore } from './initialStates'
 import Lettuce from '../themes'
 
-injectGlobal`
-    html {
-        display: flex;
-        width: 100%;
-        height: 100%;
-        font-family: 'Ropa Sans', sans-serif;
-        letter-spacing: 1px;
-        background-color: ${Lettuce.primary.dark};
-    }
-
-    }
-    body, #root {
-      display: flex;
-      flex: 1;
-    }
-    
-    body {
-      margin: 0;
-    }
-`
-
-library.add(
-    faTimes,
-    faMobileAlt,
-    faForward,
-    faBackward,
-    faPlayCircle,
-    faPauseCircle,
-)
-
-storiesOf('Playing', module)
+storiesOf('Button', module)
     .addDecorator(story => (
-        <ThemeProvider theme={Lettuce}>
-            <Provider store={createInitialStore(initialState)}>
-                {story()}
-            </Provider>
-        </ThemeProvider>
+        <ThemeProvider theme={Lettuce}>{story()}</ThemeProvider>
     ))
-    .add('component', () => <Playing />)
-
-storiesOf('Song', module)
-    .addDecorator(story => (
-        <ThemeProvider theme={Lettuce}>
-            <Provider store={createInitialStore(initialState)}>
-                {story()}
-            </Provider>
-        </ThemeProvider>
-    ))
-    .add('component', () => <SongList />)
+    .add('popover', () => <div />)
